29943c351147950b7659cdadcdcede2ce7e6394f
Artnod <artnod78@users.noreply.github.com>
2018-11-04 16:10:12 +0100

    Update discord_bot.py

--- scripts/usr/local/bin/fivembot/discord_bot.py
+++ scripts/usr/local/bin/fivembot/discord_bot.py
@@ -210,6 +210,37 @@ async def esx_brinks(ctx):
         my_logger.warn('{0.name} Bad channel'.format(member))
         await bot.say('**{0.name}** ! Bad channel!\r'.format(member))
 
+@update.command(pass_context=True)
+async def esx_journalist(ctx):
+    """
+    \tUpdate esx_journalist
+    
+    Usage: /update esx_journalist
+    """
+    my_logger.debug(sys._getframe().f_code.co_name)
+    channel = ctx.message.channel
+    member = ctx.message.author
+    if channel.id == BOT_CONF['bot_channel']:
+        if member.id in BOT_CONF['bot_admin_list']:
+            await bot.say('**{0.name}** try to update **esx_journalist**!\r'.format(member))
+            try:
+                my_logger.info('{0.name} try to update esx_journalist'.format(member))
+                p = Popen(["/root/fmm/update.sh", "-j"], stdout=PIPE)
+                toto = p.communicate()
+                if toto[0] != None:
+                    await bot.say(toto[0].decode('unicode_escape'))
+                else:
+                    await bot.say(toto[1].decode('unicode_escape'))
+            except:
+                my_logger.err('Command Failed')
+                await bot.say('Command Failed!')
+        else:
+            my_logger.warn('{0.name} Access Denied'.format(member))
+            await bot.say('**{0.name}** ! Access Denied!\r'.format(member))
+            
+    else:
+        my_logger.warn('{0.name} Bad channel'.format(member))
+        await bot.say('**{0.name}** ! Bad channel!\r'.format(member))
 
 # main
 if __name__ == '__main__':
